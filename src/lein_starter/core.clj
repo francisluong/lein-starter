cbaaf0e2555e5e1706b606f89de9d98b0db33ef1
Francis Luong (Franco) <fluong@fx-mbp-13.local>
2016-04-17 21:28:19 -0400

    destructuring maps

--- src/lein_starter/core.clj
+++ src/lein_starter/core.clj
@@ -16,6 +16,7 @@
   ([name]
    (x-chop name "karate")))
 
+;(0 or 1)-arity
 (defn weird-arity
   ([]
     "Destiny dressed you this morning,...")
@@ -26,11 +27,31 @@
   [name]
   (str "Get off my lawn, " name "!!!"))
 
+; take many args into a list
 (defn allthekids
   [& names]
   (map offmylawn names))
 
+; 1-arity plus variable-arity
 (defn favorite-things
   [name & things]
   (str "Hi, " name ", here are my favorite things: "
        (clojure.string/join ", " things)))
+
+; binding names to list positions
+(defn top-choices
+  [[first-choice second-choice & other-choices]]
+  (str "Your first choice is: " first-choice "\n"
+       "Your second choice is " second-choice "\n"
+       "We're ignoring the rest of your choices. "
+       "Here they are in case you want to cry over them: "
+       (clojure.string/join ", " other-choices)))
+
+; destructuring a map to bind names to keys
+;   :keys provides a list of keys that will be bound to the same variables
+;   :as also gives you a name for the full map
+(defn treasure-coordinates
+  [{:keys [lat lng] :as treasure-location}]
+  (str "lattitude: " lat "\n"
+       "longitude: " lng "\n"
+       "all: " treasure-location))
--- test/lein_starter/core_test.clj
+++ test/lein_starter/core_test.clj
@@ -33,3 +33,16 @@
   (testing "favorite-things"
     (is (= (favorite-things "Franco" "raindrops" "roses" "kisses" "kittens")
            "Hi, Franco, here are my favorite things: raindrops, roses, kisses, kittens"))))
+
+(deftest top-choices-test
+  (testing "top-choices"
+    (is (= (top-choices ["alpha" "bravo" "charlie" "delta"])
+           (str "Your first choice is: alpha\n"
+                "Your second choice is bravo\n"
+                "We're ignoring the rest of your choices. "
+                "Here they are in case you want to cry over them: charlie, delta")))))
+
+(deftest treasure-coordinates-test
+  (testing "treasure-coordinates"
+    (is (= (treasure-coordinates {:lat 1 :lng 2})
+           "lattitude: 1\nlongitude: 2\nall: {:lat 1, :lng 2}"))))
